5372080210adb6d2258e9fcaf08cc32ead388444
Naveen Gattu <naveen@vsco.co>
2019-01-14 14:59:07 -0800

    rm debug

--- domino_test.go
+++ domino_test.go
@@ -543,8 +543,6 @@ func TestExpressions(t *testing.T) {
 	expectedFilter := "registrationDate = :filter_1 OR contains(lastName,:filter_2) OR (NOT registrationDate = :filter_3) OR (size(visits) <=:filter_4 AND size(firstName) >=:filter_5) OR registrationDate = :filter_6 OR registrationDate <= :filter_7 OR (registrationDate between :filter_8 and :filter_9) OR (registrationDate in (:filter_10,:filter_11))"
 	assert.Equal(t, expectedFilter, *q.Build().FilterExpression)
 
-	// fmt.Println(*q.Build())
-
 	channel := make(chan *User)
 	errChan := q.ExecuteWith(ctx, db).StreamWithChannel(channel)
 
